721cf25ab62d703e6647914b6b9ba8fb7ee52a32
Sven Reimers <svenreimers@users.noreply.github.com>
2011-10-05 09:33:23 -0700

    Add PropertySheet

--- nbfx.examples/src/org/nbfx/examples/j1schedule/J1ScheduleNode.java
+++ nbfx.examples/src/org/nbfx/examples/j1schedule/J1ScheduleNode.java
@@ -5,10 +5,10 @@
 package org.nbfx.examples.j1schedule;
 
 import java.awt.Image;
+import java.lang.reflect.InvocationTargetException;
 import org.nbfx.examples.j1schedule.J1ScheduleReader.J1Session;
-import org.openide.nodes.AbstractNode;
-import org.openide.nodes.Children;
-import org.openide.nodes.Node;
+import org.nbfx.examples.node.MyNode;
+import org.openide.nodes.*;
 import org.openide.util.ImageUtilities;
 
 /**
@@ -28,8 +28,6 @@ public class J1ScheduleNode extends AbstractNode {
         return ImageUtilities.loadImage("org/nbfx/examples/j1schedule/javaone_opened.jpg");
     }
 
-    
-    
     private static class J1SessionChildren extends Children.Keys<J1Session> {
 
         @Override
@@ -45,10 +43,48 @@ public class J1ScheduleNode extends AbstractNode {
 
         private static class J1SessionNode extends AbstractNode {
 
+            final J1Session session;
+
             public J1SessionNode(J1Session t) {
                 super(Children.LEAF);
+                session = t;
                 this.setDisplayName(t.getTitle() + " @ " + t.getLocation());
                 this.setIconBaseWithExtension("org/nbfx/examples/j1schedule/duke.gif");
+            }
+
+            @Override
+            protected Sheet createSheet() {
+                final Sheet sheet = super.createSheet();
+                final Sheet.Set sheetSet = new Sheet.Set();
+
+                sheetSet.setDisplayName("Session");
+                sheetSet.setName("Session");
+
+                sheetSet.put(new PropertySupport.ReadOnly<String>("Id", String.class, "Id", "Id") {
+
+                    @Override
+                    public String getValue() throws IllegalAccessException, InvocationTargetException {
+                        return session.getId();
+                    }
+                });
+                sheetSet.put(new PropertySupport.ReadOnly<String>("Title", String.class, "Title", "Title") {
+
+                    @Override
+                    public String getValue() throws IllegalAccessException, InvocationTargetException {
+                        return session.getTitle();
+                    }
+                });
+                sheetSet.put(new PropertySupport.ReadOnly<String>("Location", String.class, "Location", "Location") {
+
+                    @Override
+                    public String getValue() throws IllegalAccessException, InvocationTargetException {
+                        return session.getLocation();
+                    }
+                });
+
+                sheet.put(sheetSet);
+
+                return sheet;
             }
         }
     }
